96f41cafa30c237a5c7b8f8851764c3d82b3c168
Hemjotkaur <hemjotkaur786@gmail.com>
2024-07-29 12:46:59 +0530

    Finished with templates

---
+++ base/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path # type: ignore
+from . import views
+
+urlpatterns = [
+  
+    path('',views.home, name='home' ),
+    path('room/', views.room),
+
+    path('room/<str:pk>/', views.room, name='room'),
+   
+    path('about/', views.about),
+    path('create-room/', views.createRoom, name='create-room'),
+    path('update-room/<str:pk>/', views.updateRoom, name='update-room'),
+    path('delete-room/<str:pk>/', views.deleteRoom, name='delete-room'),
+    path('login/',views.loginPage, name='login'),
+    path('logout/',views.logoutUser, name='logout'),
+    path('register/',views.registerPage, name='register'),
+    path('delete-message/<str:pk>', views.deleteMessage, name='delete-message'),
+    path('user-profile/<str:pk>/',views.userProfile,name='user-profile'),
+    path('update-user/',views.updateUser,name='update-user'),
+    path('setting_/',views.setting, name='settings'),
+    path('moretopic/',views.more, name='topics'),
+    path('activity/',views.activitypage, name='activity')
+
+  
+
+
+
+]
